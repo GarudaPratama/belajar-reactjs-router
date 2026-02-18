96f2aa3ec7a2691df63bd316c55ce3acc3540dae
GarudaPratama <garuda281209@student.abudzar.sch.id>
2026-02-18 09:59:59 +0700

    Done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./Home.jsx";
+import About from "./About.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
